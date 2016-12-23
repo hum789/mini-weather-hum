51628cd0d92e3ea6e642262822d6d342d98166e3
hum789 <hum_uestc@163.com>
2016-12-23 20:02:06 +0800

    V1.4

---
+++ app/src/main/java/com/pku/hum/miniweather/ViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.pku.hum.miniweather;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
+/**
+ * Created by hum on 16-11-29.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter{
+    private List<View> views;
+    private Context context;
+
+    public ViewPagerAdapter(List<View> views, Context context) {
+        this.context = context;
+        this.views = views;
+    }
+    @Override
+    public int getCount() {
+        Log.d("guide","getCount");
+        return views.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Log.d("guide","instantiateItem");
+        container.addView(views.get(position));
+        return views.get(position);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        Log.d("guide","isViewFromObject");
+        return (view == object);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(views.get(position));
+        Log.d("guide","destroyItem");
+    }
+
+}
